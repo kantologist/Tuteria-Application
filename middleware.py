5f7c585e2428e08f65a05356679a7b4778ee36f9
azeez_femi <azeezfemi17937@yahoo.com>
2016-11-12 11:26:55 +0100

    fixed versioning issues

--- middleware.py
+++
@@ -1,6 +0,0 @@
-class NoCacheMiddleware(object):
-    """Can be put anywhere in middlewares"""
-
-    def process_response(self, request, response):
-        response['Cache-Control'] = 'no-cache'
-        return response
